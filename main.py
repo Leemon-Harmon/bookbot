58fdbf94a041e19aa1962d3a1ccf83a3a88e5dae
Leemon-Harmon <leemon.harmon@gmail.com>
2024-05-31 05:09:43 -0500

    Python file now reads from text file and saves the data within it to a string

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+contents = None
+
+def main():
+    with open("books/frankenstein.txt") as file:
+        contents = file.read()
+    print(contents[:100])
+
+if __name__=="__main__":
+    main()
