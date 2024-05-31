4f8d1a76275a630762a41a674cddb923abd214c9
Leemon-Harmon <leemon.harmon@gmail.com>
2024-05-31 06:16:22 -0500

    Python file now calculates the number of words there are in the text file and calculates how many of each character are in the text file

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 contents = None
 
+def countChars(testString):
+    dict = {}
+    lowerCaseString = testString.lower()
+    for i in lowerCaseString:
+        if i in dict:
+            dict[i] += 1
+        else:
+            dict[i] = 1
+    return dict
+
+
+def totalWords(bookContent):
+    words = bookContent.split()
+    return len(words)
+
 def main():
     with open("books/frankenstein.txt") as file:
         contents = file.read()
     print(contents[:100])
+    print(totalWords(contents))
+    print(countChars(contents))
 
-if __name__=="__main__":
-    main()
+main()
